8ca44761c93b2a23843becd3354cff6c1d899445
TrAyZeN <1810leo@gmail.com>
2021-05-11 09:37:24 +0200

    Remove mut for turing machine

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use transition::{Action, Direction, State, Transition};
 use turing_machine::TuringMachine;
 
 fn main() {
-    let mut tm = TuringMachine::new([
+    let tm = TuringMachine::new([
         Transition::new(
             Action::new(1, Direction::Left, State::A),
             Action::new(0, Direction::Left, State::A),
